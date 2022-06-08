27f4455c8eb66929d74d3de34e199b940817cc0d
Eva-cm <106739519+Eva-cm@users.noreply.github.com>
2022-06-08 10:01:04 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let mySprite2: Sprite = null
+game.splash("BENVINGUTS A L'ESPAI", "Apreta A per començar i B per disparar")
+effects.starField.startScreenEffect()
+let mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 6 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 6 . . . . . . . 
+    . . . . . . 8 8 9 8 . . . . . . 
+    . . . . . . 8 6 9 8 . . . . . . 
+    . . . . . c c c 8 8 8 . . . . . 
+    . . . . 8 8 6 6 6 9 8 8 . . . . 
+    . . 8 f f f c c e e f f 8 8 . . 
+    . 8 8 8 8 8 8 6 6 6 6 9 6 8 8 . 
+    8 8 8 8 8 8 8 8 6 6 6 9 6 6 8 8 
+    8 8 8 8 8 8 8 8 6 6 6 6 9 6 8 8 
+    `, SpriteKind.Player)
+mySprite.setPosition(77, 32)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+game.onUpdateInterval(1000, function () {
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . 3 . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+})
