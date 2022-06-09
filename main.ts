9f61058cac53b8e15e2f4cb2794c778a02ab9caf
Eva-cm <106739519+Eva-cm@users.noreply.github.com>
2022-06-09 12:34:12 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,72 @@
-let mySprite2: Sprite = null
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        ................
+        .........888....
+        .......88668....
+        ......86688.....
+        .....8768.......
+        ....8778........
+        ....8778........
+        ...8778.........
+        ...8578.........
+        ...8558.........
+        ...8758......88.
+        ...87678....878.
+        ...87678...878..
+        ....87678.8768..
+        ....876768678...
+        .....87668778...
+        ......8668766...
+        .......8687678..
+        ........8667678.
+        ........8685756.
+        ....88..86665756
+        ...868..86685656
+        ..8668..86687678
+        .8668..868687678
+        .868..8688667678
+        8768.88886876778
+        8768.8888877678.
+        876688888676778.
+        87676888668778..
+        .876776868668...
+        .87766778868....
+        ..877667688.....
+        ...86767788.....
+        `, mySprite, 0, 50)
+    music.pewPew.play()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Asteroide.destroy()
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.fire, 500)
+    scene.cameraShake(4, 500)
+})
+let Asteroide: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
 game.splash("BENVINGUTS A L'ESPAI", "Apreta A per començar i B per disparar")
 effects.starField.startScreenEffect()
-let mySprite = sprites.create(img`
+mySprite = sprites.create(img`
     . . . . . . . c d . . . . . . . 
     . . . . . . . c d . . . . . . . 
     . . . . . . . c d . . . . . . . 
@@ -22,23 +87,26 @@ let mySprite = sprites.create(img`
 mySprite.setPosition(77, 32)
 controller.moveSprite(mySprite, 100, 100)
 mySprite.setStayInScreen(true)
+info.setLife(5)
 game.onUpdateInterval(1000, function () {
-    mySprite2 = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . 3 . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
+    Asteroide = sprites.createProjectileFromSide(img`
+        . . . . . . . . . c c 8 . . . . 
+        . . . . . . 8 c c c f 8 c c . . 
+        . . . c c 8 8 f c a f f f c c . 
+        . . c c c f f f c a a f f c c c 
+        8 c c c f f f f c c a a c 8 c c 
+        c c c b f f f 8 a c c a a a c c 
+        c a a b b 8 a b c c c c c c c c 
+        a f c a a b b a c c c c c f f c 
+        a 8 f c a a c c a c a c f f f c 
+        c a 8 a a c c c c a a f f f 8 a 
+        . a c a a c f f a a b 8 f f c a 
+        . . c c b a f f f a b b c c 6 c 
+        . . . c b b a f f 6 6 a b 6 c . 
+        . . . c c b b b 6 6 a c c c c . 
+        . . . . c c a b b c c c . . . . 
+        . . . . . c c c c c c . . . . . 
+        `, 0, 50)
+    Asteroide.x += randint(0, scene.screenWidth())
+    Asteroide.setKind(SpriteKind.Enemy)
 })
